17d92425b4c513860cade6796ec6f2b1c619ee0d
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-08-07 01:48:59 +0100

    added get all reviews under a post

--- src/controller/Review.js
+++ src/controller/Review.js
@@ -1,4 +1,3 @@
-const Post = require("../models/Post");
 const Review = require("../models/Review");
 
 async function Create(req, res) {
@@ -12,6 +11,15 @@ async function Create(req, res) {
   await review.save();
   res.status(200).json({
     message: "Review created successfully",
+  });
+}
+
+async function GetAll(req, res) {
+  const { postId } = req.params;
+  const reviews = await Review.find({ postId });
+  res.status(200).json({
+    message: "Reviews fetched successfully",
+    reviews,
   });
 }
 
