9a4e4ce48e6062812832dc0dd78f3ecad910b62a
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-08-07 01:51:53 +0100

    added route...

--- src/routes/PostRoute.js
+++ src/routes/PostRoute.js
@@ -2,5 +2,7 @@ const AdminPost = require("../controller/AdminPost");
 const router = require("express").Router();
 
 router.post("/create", AdminPost.Create);
+router.get("/all", AdminPost.GetAll);
+router.delete("/delete/:id", AdminPost.Delete);
 
 module.exports = router;
